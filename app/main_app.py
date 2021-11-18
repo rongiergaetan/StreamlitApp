20ecd540cf5d966d6f96614223f43dd61c5bb16e
gaetan rongier <gaetan.rongier@example.com>
2021-11-18 07:01:00 -0300

    Maj directory for read.csv function

--- app/main_app.py
+++ app/main_app.py
@@ -10,9 +10,9 @@ st.title('Stat Tirs')
 Championshipchoice = st.sidebar.selectbox("which Championship do you want ?",("Tirs_Bundesliga.csv","Tirs_PL.csv","Tirs_liga.csv","Tirs_SerieA.csv","Tirs_L1.csv") )
 
 
-choice = "../data/" + Championshipchoice
+choice = "data/" + Championshipchoice
 TableChoice = pd.read_csv(choice)
-ChampionshipName = choice.split(".")[2].split("_")[1]
+ChampionshipName = choice.split("_")[1].split(".")[0]
 
 
 figure = plot.StatButs(TableChoice,ChampionshipName)
