b5f8a9a65fb8a17f2639de69fdeef79c0bdb69e7
rongiergaetan <79275066+rongiergaetan@users.noreply.github.com>
2021-11-29 07:07:19 -0300

    Update main_app.py
    modification path to data

--- app/main_app.py
+++ app/main_app.py
@@ -10,7 +10,7 @@ st.title('Stat Tirs')
 Championshipchoice = st.sidebar.selectbox("which Championship do you want ?",("Tirs_Bundesliga.csv","Tirs_PL.csv","Tirs_liga.csv","Tirs_SerieA.csv","Tirs_L1.csv") )
 
 
-choice = "data/" + Championshipchoice
+choice = "./data/" + Championshipchoice
 TableChoice = pd.read_csv(choice)
 ChampionshipName = choice.split("_")[1].split(".")[0]
 
