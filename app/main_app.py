902e25d5c6bc969e269f0597f007fe8f29a77387
rongiergaetan <79275066+rongiergaetan@users.noreply.github.com>
2021-11-29 07:20:19 -0300

    Update main_app.py
    try subprocess

--- app/main_app.py
+++ app/main_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd 
 import sys
 import plot
+import subprocess
 
 st.title('Stat Tirs')
 
@@ -9,6 +10,9 @@ st.title('Stat Tirs')
 
 Championshipchoice = st.sidebar.selectbox("which Championship do you want ?",("Tirs_Bundesliga.csv","Tirs_PL.csv","Tirs_liga.csv","Tirs_SerieA.csv","Tirs_L1.csv") )
 
+
+x = subprocess.run(['ls'])
+print(x)
 
 choice = "/data/" + Championshipchoice
 TableChoice = pd.read_csv("/data/Tirs_Bundesliga.csv")
