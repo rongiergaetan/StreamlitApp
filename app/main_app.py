39eb7b20e8cdc9cc6380c6ac05d3da2624726b59
gaetan rongier <gaetan.rongier@example.com>
2022-02-08 08:03:16 -0300

    change name selectbox

--- app/main_app.py
+++ app/main_app.py
@@ -12,7 +12,7 @@ Championshipchoice = stl.sidebar.selectbox("which Championship do you want ?",("
 ChampionshipName = Championshipchoice.split("_")[1].split(".")[0]
 
 #choix du tableau statistique de comparaison
-StatDataTable = stl.sidebar.selectbox("which stat data table do you want ?",("stat table list", "BaseDataTeam", "AgainstBaseDataTeam", "GoalkeepingStat", "AgainstGoalkeepingStat", "AdvancedGoalkeepingStat", "AgainstAdvancedGoalkeepingStat", "DataShoots", "AgainstDataShoots", "PassStat", "AgainstPassStat", "PassStatType", "AgainstPassStatType", "OffensiveCreation", "AgainstOffensiveCreation", "DefensiveAction", "AgainstDefensiveAction", "Possession","AgainstPossession") )
+StatDataTable = stl.sidebar.selectbox("Statistic Data Table Selectbox",("which stat data table do you want ?", "BaseDataTeam", "AgainstBaseDataTeam", "GoalkeepingStat", "AgainstGoalkeepingStat", "AdvancedGoalkeepingStat", "AgainstAdvancedGoalkeepingStat", "DataShoots", "AgainstDataShoots", "PassStat", "AgainstPassStat", "PassStatType", "AgainstPassStatType", "OffensiveCreation", "AgainstOffensiveCreation", "DefensiveAction", "AgainstDefensiveAction", "Possession","AgainstPossession") )
 
 ###############################################################################
 #Classement
@@ -22,9 +22,14 @@ stl.title("Classement")
 stl.dataframe(DataFbref[0],width=1000,height=1000)
 stl.write("source: fbref")
 
-stl.write("\t\t")
 
  #tableau statistique
-stl.title(StatDataTable)
-stl.dataframe(DataFbref[st.Dico_OutDatafBreb[StatDataTable]],width=1000,height=1000)
-stl.write("source: fbref")
+stl.title("Statistic")
+
+if StatDataTable == "which stat data table do you want ?":
+	stl.write("which stat data table do you want ?")
+	stl.write(" choice in the sidedebar with the second selectbox")
+else:
+	stl.write(StatDataTable)
+	stl.dataframe(DataFbref[st.Dico_OutDatafBreb[StatDataTable]],width=1000,height=1000)
+	stl.write("source: fbref")
